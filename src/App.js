832e8736cd3269ebd148292c1271c422abebf744
Akash Chanda Tushar <akash.tushar98@gmail.com>
2022-05-22 12:48:46 +0600

    basic setup done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Navigation from "./Components/Navigation";
+import Footer from "./Components/Footer";
+import Home from "./Pages/Home/Home";
+import SignIn from "./Pages/UserAuth/SignIn";
+import SignUp from "./Pages/UserAuth/SignUp";
+import ResetPass from "./Pages/UserAuth/ResetPass";
+import FourZeroFour from "./Components/FourZeroFour";
+import DashBoard from "./Pages/DashBoard/DashBoard";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navigation />
+            <Routes>
+                <Route path="/">
+                    <Route index element={<Home />} />
+                    <Route path="/sign-in" element={<SignIn />} />
+                    <Route path="/sign-up" element={<SignUp />} />
+                    <Route path="/reset-pass" element={<ResetPass />} />
+                    <Route path="/dashboard" element={<DashBoard />} />
+
+                    <Route path="*" element={<FourZeroFour />} />
+                </Route>
+            </Routes>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
